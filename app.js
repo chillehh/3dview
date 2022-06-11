233ee5143ad3d05229facb1c944459c248963366
Jacob Ling <jacobrling@gmail.com>
2022-06-11 16:59:50 +0930

    Added a router to control routes

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const session = require('express-session');
 const MongoDBSession = require('connect-mongodb-session')(session);
 
 const controller = require('./controllers/controller.js');
-app.use(express.static(__dirname + '/public'));
+const router = require('./routes/router')
 
-let indexFile;
+app.use(express.static(__dirname + '/public'));
 
 const {MongoClient} = require('./db.js');
 
@@ -31,65 +31,26 @@ app.use(fileUpload({
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/', (req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    res.writeHead(200);
-    res.end(indexFile);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+express.json();
+express.urlencoded();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-fs.readFile(__dirname + "/index.html")
-    .then(contents => {
-        indexFile = contents;
-        app.listen(port, host, () => {
-            console.log(`Server is running on http://${host}:${port}`);
-        });
+app.use(express.static(path.join(__dirname, '../views')));
+app.use(express.static(path.join(__dirname, '../public')));
 
-    })
-    .catch(err => {
-        console.error(`Could not read index.html file: ${err}`);
-        process.exit(1);
-    });
-
-app.post('/upload', async (req, res) => {
-    try {
-        if(!req.files) {
-            res.send({
-                status: false,
-                message: 'No file uploaded'
-            });
-        } else {
-
-            let file = req.files.fileInput;
-            // Split by all dots, grab last element in array
-            let dots = _.split(file.name, '.')
-            let extension = dots[dots.length - 1]
-            if (extension !== undefined && extension === 'obj') {
-                console.log('HAVE extension, > add to db')
-                let id = uuid();
-                console.log(id)
-                const newModelData = new ModelData({
-                    dataId: id,
-                    path: "./uploads/" + file.name,
-                })
-                await newModelData.save();
-            }
-            file.mv('./uploads/' + file.name);
-
-            // send response to the upload route
-            // TODO: redirect to 3D viewer page with response data
-            res.send({
-                status: true,
-                message: 'File is uploaded',
-                data: {
-                    name: file.name,
-                    mimetype: file.mimetype,
-                    size: file.size
-                }
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
-    }
+app.use('/', router)
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
